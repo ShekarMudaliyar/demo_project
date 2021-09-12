3a806eabfc718b5b6a500a6af99925fcdf53cab5
Shekar Mudaliyar <sskshekar10@gmail.com>
2021-09-13 03:36:21 +0530

    added widget testing

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,23 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:demo_project/homepage/postspage.dart';
+import 'package:demo_project/models/usersmodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:demo_project/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Testing create post navigator from posts list page',
+      (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MaterialApp(home: PostPage(user: Users(id: '1'))));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    var button = find.byType(FloatingActionButton);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.tap(button);
+    await tester.pumpAndSettle();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('Posts'), findsNothing);
+    expect(find.text('Add Post'), findsWidgets);
   });
 }
